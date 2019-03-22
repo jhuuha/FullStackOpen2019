59b5e787b3a84e2862ebdb083ad85d494da752e0
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-22 19:28:08 +0200

    2.5: erillinen moduuli

---
+++ osa02/02_01-05_kurssitiedot/src/components/Course.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <h1>{props.name}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+}
+
+const Content = ({ parts }) => {
+
+    const rows = () => parts.map(part =>
+        <Part key={part.id} part={part} />
+    )
+
+    return (
+        <div>
+            {rows()}
+        </div>
+    )
+}
+
+const Total = ({ parts }) => {
+
+    const total = parts.reduce((s, p) => s + p.exercises, 0)
+
+    return (
+        <p>yhteensä {total} tehtävää</p>
+    )
+}
+
+const Course = ({ course }) => {
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+export default Course
--- osa02/02_01-05_kurssitiedot/src/index.js
+++ osa02/02_01-05_kurssitiedot/src/index.js
@@ -1,51 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Header = (props) => {
-    return (
-        <h1>{props.name}</h1>
-    )
-}
-
-const Part = (props) => {
-    return (
-        <p>
-            {props.part.name} {props.part.exercises}
-        </p>
-    )
-}
-
-const Content = ({ parts }) => {
-
-    const rows = () => parts.map(part =>
-        <Part key={part.id} part={part} />
-    )
-
-    return (
-        <div>
-            {rows()}
-        </div>
-    )
-}
-
-const Total = ({ parts }) => {
-
-    const total = parts.reduce((s, p) => s + p.exercises, 0)
-
-    return (
-        <p>yhteensä {total} tehtävää</p>
-    )
-}
-
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
-}
+import Course from './components/Course'
 
 const App = () => {
 
