e8de6f822de5b94ed7de505cee785b6bdc8d0a44
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-23 15:01:11 +0200

    2.15: puhelinluettelo step7

---
+++ osa02/02_15-20_puhelinluettelo/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons';
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (newName.length > 0) {
+      if (!(persons.some(person => person['name'] === newName))) {
+        const personObject = {
+          name: newName,
+          number: newNumber
+        }
+        axios
+          .post('http://localhost:3001/persons', personObject)
+          .then(response => {
+            setPersons(persons.concat(personObject))
+            setNewName('')
+            setNewNumber('')
+          })
+      } else {
+        window.alert(`${newName} on jo luettelossa`)
+      }
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+      <h3>lisää uusi</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numerot</h3>
+      <Persons persons={persons} newFilter={newFilter} />
+    </div>
+  )
+
+}
+
+export default App
