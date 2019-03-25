ae0a224c8a860a450979d6621ca770af1a3c3e7a
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-25 18:02:46 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- osa04/04_03-07_apufunktioita_ja_yksikkotesteja/utils/list_helper.js
+++ osa04/04_03-07_apufunktioita_ja_yksikkotesteja/utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if (blogs === undefined || blogs.lenght === 0) {
+        return 0
+    }
+    return blogs.reduce((s, p) => s + p.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
