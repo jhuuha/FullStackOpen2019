a0bf655b6a59774a6f834a74b270e6f694497be1
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-28 08:48:41 +0200

    5.2: blogilistan frontend, step2

--- osa05/05_01-12_blogilistan_frontend/src/App.js
+++ osa05/05_01-12_blogilistan_frontend/src/App.js
@@ -18,14 +18,32 @@ const App = () => {
   }, [])
 
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+
   const handleLogin = async (event) => {
     event.preventDefault()
     const user = await loginService.login({
       username, password,
     })
+    window.localStorage.setItem(
+      'loggedBlogappUser', JSON.stringify(user)
+    )
     setUser(user)
     setUsername('')
     setPassword('')
+  }
+
+
+  const handleLogout = async (event) => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
 
@@ -63,6 +81,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <p>{user.name} logged in</p>
+      <button onClick={() => handleLogout()}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
