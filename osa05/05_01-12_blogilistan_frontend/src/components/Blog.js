b61ed3cde4d7569a28ee6b2d5ab088671fc45d57
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-28 13:10:10 +0200

    5.6* blogilistan frontend, step6

--- osa05/05_01-12_blogilistan_frontend/src/components/Blog.js
+++ osa05/05_01-12_blogilistan_frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    color: 'black',
+    border: '1px solid black',
+    margin: '5px 0px',
+    paddingTop: '10px',
+    paddingLeft: '2px',
+    borderRadius: '2px',
+    userSelect: 'none'
+  }
+
+  if (!(showDetails)) {
+    return (<div style={blogStyle}>
+      <div onClick={() => setShowDetails(true)}>{blog.title} {blog.author}</div>
+    </div>)
+  } else {
+    const user = blog.user === undefined ? '' : `added by ${blog.user.username}`
+    return (<div style={blogStyle}>
+      <div onClick={() => setShowDetails(false)}>{blog.title} {blog.author}<br /></div>
+      <a href={blog.url}>{blog.url}</a><br />
+      {blog.likes} likes <button>like</button><br />
+      {user}
+    </div>)
+  }
+}
 
 export default Blog
