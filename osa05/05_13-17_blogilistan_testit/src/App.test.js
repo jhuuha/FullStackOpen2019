11d8c36691267dfcc1eacb6faac5446c5fa66a83
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-28 21:15:44 +0200

    5.17*: blogilistan testit, step5

--- osa05/05_13-17_blogilistan_testit/src/App.test.js
+++ osa05/05_13-17_blogilistan_testit/src/App.test.js
@@ -5,14 +5,12 @@ import App from './App'
 describe('<App />', () => {
     it('if no user logged, notes are not rendered', async () => {
 
-        /*
         const user = {
             username: 'tester',
             token: '1231231214',
             name: 'Teuvo Testaaja'
         }
         localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
-        */
 
         const component = render(
             <App />
@@ -20,18 +18,17 @@ describe('<App />', () => {
         component.rerender(<App />)
 
         await waitForElement(
-            () => component.getByText('kirjaudu')
+            () => component.getByText('logout')
         )
 
         // expectations here
-        const elementTitle = component.getByText('Log in to application')
+        const elementTitle = component.getByText('blogs')
         expect(elementTitle).toBeDefined()
 
-        const elementLogin = component.getByText('kirjaudu')
-        expect(elementLogin).toBeDefined()
+        const elementLogout = component.getByText('logout')
+        expect(elementLogout).toBeDefined()
 
-        expect(component.container).not.toHaveTextContent('blogs')
-        expect(component.container).not.toHaveTextContent('Testi Simo')
-        expect(component.container).not.toHaveTextContent('Testi2 Timo')
+        expect(component.container).toHaveTextContent('Testi Simo')
+        expect(component.container).toHaveTextContent('Testi2 Timo')
     })
 })
