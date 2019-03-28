7e0e2614a8985538b4618b9b3ee21257128e3aad
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-28 18:24:05 +0200

    5.14: blogilistan testit, step2

--- osa05/05_13-17_blogilistan_testit/src/components/SimpleBlog.test.js
+++ osa05/05_13-17_blogilistan_testit/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'jest-dom/extend-expect'
-import { render, cleanup } from 'react-testing-library'
+import { render, fireEvent, cleanup } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 afterEach(cleanup)
@@ -23,8 +23,6 @@ test('renders content', () => {
         <SimpleBlog blog={blog} />
     )
 
-    component.debug()
-
     const title = component.container.querySelector('.title')
     expect(title).toHaveTextContent(
         'Testi blogi Simo Salminen'
@@ -34,5 +32,31 @@ test('renders content', () => {
     expect(likes).toHaveTextContent(
         'blog has 5 likes'
     )
+})
 
+it('two clicking the button calls event handler two times', async () => {
+    const blog = {
+        title: 'Testi blogi',
+        author: 'Simo Salminen',
+        url: 'http://testiblogi.fi',
+        user: {
+            username: 'root',
+            name: 'super user',
+            id: '5c9bc3d228313c68cb937d00'
+        },
+        likes: 5,
+        id: '5c9c78839494eb3857392ddb'
+    }
+
+    const mockHandler = jest.fn()
+
+    const { getByText } = render(
+        <SimpleBlog blog={blog} onClick={mockHandler} />
+    )
+
+    const button = getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls.length).toBe(2)
 })
