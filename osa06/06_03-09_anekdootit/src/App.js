169fd497f45105572687a6f3d4e81a8a23edd472
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-30 18:01:06 +0200

    6.8: anekdootit, step6

--- osa06/06_03-09_anekdootit/src/App.js
+++ osa06/06_03-09_anekdootit/src/App.js
@@ -1,32 +1,13 @@
-import React from 'react';
+import React from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
-import { incVoteOf } from './reducers/anecdoteReducer'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = (props) => {
-  const anecdotes = props.store.getState()
-
-  const vote = (id) => {
-    console.log('vote', id)
-    props.store.dispatch(
-      incVoteOf(id)
-    )
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
       <AnecdoteForm store={props.store} />
+      <AnecdoteList store={props.store} />
     </div>
   )
 }
---
+++ osa06/06_03-09_anekdootit/src/components/AnecdoteList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { incVoteOf } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = (props) => {
+
+    const anecdotes = props.store.getState()
+
+    const vote = (id) => {
+        console.log('vote', id)
+        props.store.dispatch(
+            incVoteOf(id)
+        )
+    }
+
+    return (
+        <div>
+            <br />
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => vote(anecdote.id)}>vote</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default AnecdoteList
