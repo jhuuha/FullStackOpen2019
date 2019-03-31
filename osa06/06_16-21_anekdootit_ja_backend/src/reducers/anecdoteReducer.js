d7aa96620a33eed49556887acf094d2ff92fe7e2
Jarkko Huuha <jarkko.huuha@gmail.com>
2019-03-31 21:28:33 +0300

    6.18 anekdootit ja backend, step4

--- osa06/06_16-21_anekdootit_ja_backend/src/reducers/anecdoteReducer.js
+++ osa06/06_16-21_anekdootit_ja_backend/src/reducers/anecdoteReducer.js
@@ -1,3 +1,4 @@
+import anecdoteService from '../services/anecdotes'
 
 const reducer = (state = [], action) => {
   console.log('state now: ', state)
@@ -29,10 +30,13 @@ export const createAnecdote = (data) => {
   }
 }
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes,
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    })
   }
 }
 
